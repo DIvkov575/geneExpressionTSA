ca4bf48f8ebf4cf58eb971409be12c0313e7141e
Divkov575 <dmitriy@ivkov.net>
2025-11-10 11:22:27 -0500

    great lightgbm

--- temp_freq_calc.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("/Users/dmitriyivkov/programming/protien-tsa/data/CRE.csv")
-time_axis = pd.to_numeric(df['time-axis'])
-time_diffs = time_axis.diff().dropna()
-average_diff = time_diffs.mean()
-print(f"Average time difference: {average_diff} seconds")
